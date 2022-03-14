90826c860e38e9686dcb18a8893ba0068b148543
Ayoub Eleuch <58662566+AyoubElleuch@users.noreply.github.com>
2022-03-14 23:35:34 +0100

    Create  IOI '09 P5 - Garage.cpp

---
+++ IOI '09 P5 - Garage.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define fi first
+#define se second
+#define inf 1000000000
+#define NIL -1;
+#define sz(x) (ll)x.size()
+#define boost() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef vector<int> vi;
+typedef vector<float> vf;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<int, bool> pib;
+typedef vector<pii> vii;
+typedef vector<pib> vib;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+typedef vector<string> vs;
+typedef pair<string, string> pss;
+typedef pair<ll, ll> pll;
+typedef vector<pss> vss;
+typedef pair<db, db> pdd;
+typedef vector<db> vd;
+typedef vector<vib> vvib;
+
+int n, m;
+int garages[101];
+vi occupied(101, 0);
+int weights[2001];
+vi cars;
+unordered_map<int, int> dp;
+
+int main(){
+    ull ans = 0;
+    cin >> n >> m;
+    for(int i=1; i<=n; i++){
+        cin >> garages[i];
+    }
+    for(int i=1; i<=m; i++){
+        cin >> weights[i];
+    }
+    for(int i=0; i<m*2; i++){
+        bool found = 0;
+        int x;
+        cin >> x;
+        if(x>0){
+            for(int j=1; j<=n; j++){
+                if(!occupied[j]){
+                    found = 1;
+                    ans+=weights[x]*garages[j];
+                    dp[x]=j;
+                    occupied[j]=1;
+                    break;
+                }
+            }
+            if(!found) cars.pb(x);
+        }
+        else if(x<0){
+            occupied[dp[-x]]=0;
+            if(!cars.empty()){
+                dp[cars[0]] = dp[-x];
+                ans+=weights[cars[0]]*garages[dp[cars[0]]];
+                occupied[dp[cars[0]]] = 1;
+                cars.erase(cars.begin());
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
+// TATAKAE TATAKAE1
